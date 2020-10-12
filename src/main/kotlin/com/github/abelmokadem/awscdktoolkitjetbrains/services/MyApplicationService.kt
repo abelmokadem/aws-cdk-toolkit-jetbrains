a3c9cfeda2dee9584e7c49d0318f3d24f92c96de
GitHub Action <action@github.com>
2020-10-12 18:41:26 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/abelmokadem/awscdktoolkitjetbrains/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.abelmokadem.awscdktoolkitjetbrains.services
+
+import com.github.abelmokadem.awscdktoolkitjetbrains.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
